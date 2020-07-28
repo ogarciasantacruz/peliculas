63a2581eb364606fa9ae128cedb97cb1b1593b26
Alejandro Garcia Santacruz <alejandro@MacBook-Pro-de-Alejandro.local>
2020-07-28 17:03:18 -0500

    Se integra el login básico de laravel

---
+++ database/seeds/UserSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\User;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $admin = User::create([
+            'name'            => 'Alejandro',
+            'last_name'       => 'García Santacruz',
+            'email'           => 'ogarciasantacruz@gmail.com',
+            'password'        => bcrypt('Al3x39722#'),
+        ]);
+    }
+}
